88424c503816e1eb6850a17ca3fe80a74763d3ca
miamano <frinyo@hotmail.com>
2016-02-29 11:51:30 +0100

    First commit

---
+++ GameOfLife/GameOfLife/LifeGame.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GameOfLife3
+{
+    class LifeGame
+    {
+        private const int SLEEP = 100;
+        private const int POPULATION_DEAD = 0;
+        private const int POPULATION_DECREASE = 1;
+        private const int POPULATION_STAGNATE = 2;
+        private const int POPULATION_INCREASE = 3;
+        private const int POPULATION_START= 4;
+
+        private LifeBoard currentGeneration;   
+        private LifeUI lifeUI;
+        private int maxGeneration; 
+        private int roundGenerationCounter; //räknar antalet generation i EN runda
+        private int allGenerationCounter; //count all the generation in all round
+
+        public LifeGame(int row, int col, int maxGeneration) 
+        {
+            this.maxGeneration = maxGeneration;
+            roundGenerationCounter = 1;
+            allGenerationCounter = 1;
+
+            currentGeneration = new LifeBoard(row, col); 
+            lifeUI = new LifeUI();
+            currentGeneration.CreateBoard();
+            lifeUI.DrawBoard(currentGeneration, allGenerationCounter, POPULATION_START, currentGeneration.GetNbrOfLivingCellOfBoard());
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            bool isContinue = true;
+            while (isContinue)
+            {
+                int neighbours = 0;
+                LifeBoard newGeneration = new LifeBoard(currentGeneration.Row, currentGeneration.Column);
+                for (int row = 0; row < currentGeneration.Row; row++)
+                {
+                    for (int col = 0; col < currentGeneration.Column; col++)
+                    {
+                        neighbours = currentGeneration.CountCellsNeighbours(row, col);
+                        newGeneration.SetCellState(row, col, Rules(neighbours, currentGeneration.GetCellState(row, col)));
+                    }
+                }
+                roundGenerationCounter++;
+                allGenerationCounter++;
+
+                bool dead = !newGeneration.IsBoardInLife() ? true : false;
+                bool stagnate = CompareGenerations(currentGeneration, newGeneration) ? true : false;
+
+                if (roundGenerationCounter <= maxGeneration)
+                {
+                    if (dead || stagnate)
+                    {
+                        isContinue = false;
+                        Thread.Sleep(SLEEP);
+                        currentGeneration = newGeneration;
+                        lifeUI.DrawBoard(currentGeneration, 
+                                        allGenerationCounter, 
+                                        dead ? POPULATION_DEAD : POPULATION_STAGNATE, 
+                                        currentGeneration.GetNbrOfLivingCellOfBoard());
+                    }
+                    else
+                    {
+                        isContinue = true;
+                    }
+                }
+                else
+                {
+                    roundGenerationCounter = 1;
+                    Console.WriteLine("Denna runda är över.");
+                    Console.Write("Press <enter> for new generation or (Q)uit.");
+                    isContinue = Console.ReadLine().ToUpper() == "Q" ? false : true;                   
+                }
+
+                if (isContinue)
+                {
+                    int prev = 0;
+                    int next = 0;
+                    int state = 0;
+
+                    prev = currentGeneration.GetNbrOfLivingCellOfBoard();
+                    next = newGeneration.GetNbrOfLivingCellOfBoard();
+                    if (prev < next)
+                        state = POPULATION_INCREASE;
+                    else if (prev == next)
+                        state = POPULATION_STAGNATE;
+                    else
+                        state = POPULATION_DECREASE;
+
+                    Thread.Sleep(SLEEP);
+                    currentGeneration = newGeneration;
+                    lifeUI.DrawBoard(currentGeneration, allGenerationCounter, state, currentGeneration.GetNbrOfLivingCellOfBoard());
+                }
+            }
+        }
+
+        private bool CompareGenerations(LifeBoard current, LifeBoard next)
+        {
+            if (current.Row != next.Row && current.Column != next.Column) return false;
+
+            for (int row = 0; row < current.Row; row++)
+            {
+                for (int col = 0; col < current.Column; col++)
+                {
+                    if (current.GetCellState(row, col) != next.GetCellState(row, col)) return false;
+                }
+            }
+            return true;
+        }
+
+        private LifeBoard.State Rules(int neighbours, LifeBoard.State life)
+        {
+            if (life == LifeBoard.State.Dying) life = LifeBoard.State.Dead;
+            if (life == LifeBoard.State.Reborn) life = LifeBoard.State.Living;
+
+            if ((life == LifeBoard.State.Living) && neighbours < 2) return LifeBoard.State.Dying;
+            if ((life == LifeBoard.State.Living) && (neighbours == 2 || neighbours == 3)) return LifeBoard.State.Living;
+            if ((life == LifeBoard.State.Living) && neighbours > 3) return LifeBoard.State.Dying;
+            if ((life == LifeBoard.State.Dead) && neighbours == 3) return LifeBoard.State.Reborn;
+
+            return life; 
+        }
+    }
+}
---
+++ GameOfLife/GameOfLife/LifeUI.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife3
+{
+    class LifeUI
+    {
+        private const string POPULATION_DEAD = "DEAD"; 
+        private const string POPULATION_DECREASE = "DECREASE";
+        private const string POPULATION_STAGNATE = "STAGNATE";
+        private const string POPULATION_INCREASE = "INCREASE";
+        private const string POPULATION_START = "NOTHING TO COMPARE WITH";
+
+        private string state = string.Empty;
+ 
+        public void DrawBoard(LifeBoard board, int currGeneration, int populationState, int livingCells)
+        {
+            switch (populationState)
+            {
+                case 0:
+
+                    state = POPULATION_DEAD;
+                    break;
+                case 1:
+                    state = POPULATION_DECREASE;
+                    break;
+                case 2:
+                    state = POPULATION_STAGNATE;
+                    break;
+                case 3:
+                    state = POPULATION_INCREASE;
+                    break;
+                case 4:
+                    state = POPULATION_START;
+                    break;
+
+                default:
+                    state = "00";
+                    break;
+            }
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.Write("Generation: {0} | Living:  {1} | State: {2}", currGeneration, livingCells, state); 
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            //Simple utskrift
+            for (int row = 0; row < board.Row; row++)
+            {
+                for (int col = 0; col < board.Column; col++)
+                {
+                    string tmp = String.Empty;
+                    switch (board.GetCellState(row, col))
+                    {
+                        case LifeBoard.State.Living:
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            tmp = "x";
+                            break;
+
+                        case LifeBoard.State.Reborn:
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            tmp = "+";
+                            break;
+
+                        case LifeBoard.State.Dying:
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            tmp = "o";
+                            break;
+
+                        case LifeBoard.State.Dead:
+                            //Console.BackgroundColor = ConsoleColor.White;
+                            tmp = " ";
+                            break;
+
+                        default:
+                            tmp = "-";
+                            break;
+
+                    }
+                    
+                    Console.Write("{0}", tmp);
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+        }
+
+        public static void Welcome()
+        {
+            Console.Clear(); 
+
+            Console.WriteLine();
+            
+            string title = "Game of LIFE";
+            Console.WriteLine(title);
+            DrawLine(title.Length);
+            Console.WriteLine();
+            Console.WriteLine("Enter the preferd size (max. {0}) of the gameboard and the number of generations that will be shown at the time.", Program.MAX_SIZE);
+            Console.WriteLine();
+        }
+
+        public static void DrawLine(int length)
+        {
+            for (int i = 0; i < length - 1; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("-");
+        }
+    }
+}
